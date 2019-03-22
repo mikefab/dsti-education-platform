fc842540f8accc6d4a1e7641262860ad41626b59
Marcella <marcellamaki@gmail.com>
2019-03-22 15:12:01 +0000

    update footer styling

--- education_platform/src/Footer.js
+++ education_platform/src/Footer.js
@@ -10,14 +10,14 @@ const styles = (theme) => ({
     fontSize: '12px',
     lineHeight: '1.5',
   }
-}) 
+})
 const Footer = (props) => (
   <div className="Footer">
     <h4>Get in touch</h4>
     <p>
       State House<br></br>
       Call: +232-77-00-00-00<br></br>
-      Email: <a href="mailto:info@dsti.gov.sl?subject=Education%20Platform%20Inquiry">info@dsti.gov.sl</a><br></br>
+      Email: <a href="mailto:info@dsti.gov.sl?subject=Education%20Platform%20Inquiry" style={{ color:'#8e8e8e' }}>info@dsti.gov.sl</a><br></br>
       © {(new Date()).getFullYear()} Directorate of Science, Technology, and Innovation
     </p>
   </div>
