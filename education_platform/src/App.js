7dc34ee455aeb05ae92f8b56d8a3363c47d9fe1a
Marcella <marcellamaki@gmail.com>
2019-03-22 13:19:39 +0000

    substitute out text for header, footer, and section components

--- education_platform/src/App.js
+++ education_platform/src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
+import Section from './Section';
+import Header from './Header';
+import Footer from './Footer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h2>
-            Education Platform
-          </h2>
-        </header>
+        <Header />
+        <Section className="body"/>
+        <Footer />
       </div>
     );
   }
