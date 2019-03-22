90c713574edea4fefc67271b0ca087d812059c23
Marcella <marcellamaki@gmail.com>
2019-03-22 13:18:10 +0000

    create placeholder for section component

---
+++ education_platform/src/Section.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Section = (props) => (
+  <div className="Section">
+    <h4>Here is placeholder text for a section title</h4>
+  </div>
+)
+export default Section;
