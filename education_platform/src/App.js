8d689b2c59f71dd6de3c7b45d4b94cf2d7226e8e
Marcella <marcellamaki@gmail.com>
2019-03-25 11:59:26 +0000

    first layer data abstraction for dynamic updating

--- education_platform/src/App.js
+++ education_platform/src/App.js
@@ -2,26 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import Toolbar from '@material-ui/core/Toolbar';
 import IconButton from '@material-ui/core/IconButton';
-import SearchIcon from '@material-ui/icons/Search';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid';
 import GridList from '@material-ui/core/GridList';
 import GridListTile from '@material-ui/core/GridListTile';
 import GridListTileBar from '@material-ui/core/GridListTileBar';
 import ListSubheader from '@material-ui/core/ListSubheader';
-import InfoIcon from '@material-ui/icons/Info';
-
-// import Card from '@material-ui/core/Card';
-// import CardContent from '@material-ui/core/CardContent';
-// import CardMedia from '@material-ui/core/CardMedia';
-// import Hidden from '@material-ui/core/Hidden';
-import Button from '@material-ui/core/Button';
-// import Divider from '@material-ui/core/Divider';
-import MenuIcon from '@material-ui/icons/Menu';
 import Footer from './Footer';
+import Header from './Header';
 
 
 const styles = theme => ({
@@ -39,22 +26,11 @@ const styles = theme => ({
   },
   gridListTile: {
     margin: '25px',
-    backgroundColor: '#38568c',
+    backgroundColor: '#d8d8d8',
     maxWidth: '315px',
   },
-  toolbarTitle: {
-     flex: 1,
-     textAlign: 'left',
-     padding: '30px',
-   },
-   toolbarMain: {
-     backgroundColor: '#fcfcfc',
-   },
   icon: {
     color: 'rgba(255, 255, 255, 0.54)',
-  },
-  menuIcon: {
-    marginLeft: '20px',
   },
   footer: {
     backgroundColor: '#424242',
@@ -63,128 +39,93 @@ const styles = theme => ({
     fontSize: '12px',
     lineHeight: '1.5',
     padding: '3px',
+    marginTop: '100px',
   },
   Subheader: {
     fontSize: '20px',
     fontColor: '#424242',
   },
+  addSubheader: {
+    fontSize: '20px',
+    fontColor: '#424242',
+    textAlign: 'center',
+  },
 });
 
-const tileData = [
-  {
-    title: 'Card 1',
-    description:
-      'This is a demo card',
-    permissions: ['update', 'delete'],
-    action: 'VISIT',
-  },
-  {
-    title: 'Card 2',
-    description:
-      'This is a demo card',
-    permissions: ['update', 'delete'],
-    action: 'VISIT',
-  },
-  {
-    title: 'Card 3',
-    description:
-      'This is a demo card',
-    permissions: ['update', 'delete'],
-    action: 'VISIT',
-  },
-  {
-    title: 'Card 4',
-    description:
-      'This is a demo card',
-    permissions: ['update', 'delete'],
-    action: 'VISIT',
-  },
-  {
-    title: 'Card 5',
-    description:
-      'This is a demo card',
-    permissions: ['update', 'delete'],
-    action: 'VISIT',
-  },
-  {
-    title: 'Card 6',
-    description:
-      'This is a demo card',
-    permissions: ['update', 'delete'],
-    action: 'VISIT',
-  },
-  {
-    title: 'Add',
-    permissions: [],
-    description:
-      'Click here to add a card',
-    img: '/add_icon.png',
-  },
-];
-
 const social = ['GitHub', 'Twitter', 'Facebook'];
+const data = fetch('/data.json');
 
-function App(props) {
-  const { classes } = props;
+class App extends React.Component {
+  constructor(props) {
+   super(props);
+   this.state = {
+     data: {},
+   };
+ }
 
-  return (
-    <React.Fragment>
-      <CssBaseline />
-      <div className={classes.layout}>
-        <Toolbar className={classes.toolbarMain}>
-          <Typography
-            component="h2"
-            variant="h5"
-            color="inherit"
-            align="center"
-            noWrap
-            className={classes.toolbarTitle}
-          >
-            Education Platform
-          </Typography>
-          <Button variant="outlined" size="small">
-            Log In
-          </Button>
-          <MenuIcon className={classes.menuIcon}/>
-        </Toolbar>
-        <main>
-          {/* Sub featured posts */}
-          <div className={classes.root}>
-            <GridList cellHeight={300} className={classes.gridList} cols={3}>
-              <GridListTile key="Subheader" cols={3} style={{ height: 'auto' }}>
-                <ListSubheader component="div" className={classes.Subheader}>Education News and Information</ListSubheader>
-              </GridListTile>
-              {tileData.map(tile => (
-                <GridListTile key={tile.img} className={classes.gridListTile}>
-                  { tile.title === "Add" ?
-                    <img src={'./add_icon.png'} alt={tile.title} />
-                    :
-                    <img src={'./chart_icon.png'} alt={tile.title} />
-                  }
-                  <GridListTileBar
-                    title={tile.title}
-                    subtitle={<span>{tile.description}</span>}
-                    actionIcon={
-                      <IconButton className={classes.icon}>
-                        {tile.action}
-                      </IconButton>
-                    }
-                    className={classes.tileBar}
-                  />
+  componentWillMount() {
+    fetch('/data.json')
+    .then(res => res.json())
+    .then(json => this.setState({ data: json }))
+  }
+
+
+  render() {
+    const { classes } = this.props;
+    const { data } = this.state;
+    console.log(data)
+
+    return (
+      <React.Fragment>
+        <CssBaseline />
+        <div className={classes.layout}>
+          <Header />
+          <main>
+            {/* Sub featured posts */}
+            <div className={classes.root}>
+              <GridList cellHeight={300} className={classes.gridList} cols={3}>
+                <GridListTile key="Subheader" cols={3} style={{ height: 'auto' }}>
+                  <ListSubheader component="div" className={classes.Subheader}>Education News and Information</ListSubheader>
                 </GridListTile>
-              ))}
-            </GridList>
-          </div>
-          {/* End sub featured posts */}
-        </main>
-      </div>
-      {/* Footer */}
-      <footer className={classes.footer}>
-        <Footer />
-      </footer>
-      {/* End footer */}
-    </React.Fragment>
-  );
+                {data.tiles && data.tiles.map(tile => (
+                  <GridListTile key={tile.img} className={classes.gridListTile}>
+                    { tile.title === "Add" ?
+                      <img src={'./add_icon.png'} alt={tile.title} />
+                      :
+                      <img src={'./chart_icon.png'} alt={tile.title} />
+                    }
+                    <GridListTileBar
+                      title={tile.title}
+                      subtitle={<span>{tile.description}</span>}
+                      actionIcon={
+                        <IconButton className={classes.icon}>
+                          {tile.action}
+                        </IconButton>
+                      }
+                      className={classes.tileBar}
+                    />
+                  </GridListTile>
+                ))}
+                <GridListTile key="AddSubheader" cols={3} style={{ height: 'auto' }}>
+                  <span>
+                    <hr></hr>
+                    <ListSubheader component="div" className={classes.addSubheader}>Add A Section + </ListSubheader>
+                    <hr></hr>
+                  </span>
+                </GridListTile>
+              </GridList>
+            </div>
+            {/* End sub featured posts */}
+          </main>
+        </div>
+        {/* Footer */}
+        <footer className={classes.footer}>
+          <Footer />
+        </footer>
+        {/* End footer */}
+      </React.Fragment>
+    );
+  }
 }
 
 App.propTypes = {
--- education_platform/src/Header.js
+++ education_platform/src/Header.js
@@ -1,10 +1,46 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuIcon from '@material-ui/icons/Menu';
+import Button from '@material-ui/core/Button';
 
-const Header = (props) => (
-  <div className="Header">
-    <h2>
+const styles = theme => ({
+  toolbarTitle: {
+     flex: 1,
+     textAlign: 'left',
+     padding: '30px',
+   },
+   toolbarMain: {
+     backgroundColor: '#fcfcfc',
+   },
+   menuIcon: {
+     marginLeft: '20px',
+   },
+ })
+
+const Header = ({ classes }) => (
+  <Toolbar className={classes.toolbarMain}>
+    <Typography
+      component="h2"
+      variant="h5"
+      color="inherit"
+      align="center"
+      noWrap
+      className={classes.toolbarTitle}
+    >
       Education Platform
-    </h2>
-  </div>
+    </Typography>
+    <Button variant="outlined" size="small">
+      Log In
+    </Button>
+    <MenuIcon className={classes.menuIcon}/>
+  </Toolbar>
 )
-export default Header;
+
+Header.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Header);
